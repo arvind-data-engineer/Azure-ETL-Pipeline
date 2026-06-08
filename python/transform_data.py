8dc4177fc5af8b5d81180298db33fa44389f5179
ARVIND <carvind35@gmail.com>
2026-06-08 10:27:20 +0530

    Enhance data cleaning process and update print message

--- python/transform_data.py
+++ python/transform_data.py
@@ -2,10 +2,17 @@ import pandas as pd
 
 df = pd.read_csv("../datasets/sales.csv")
 
+# Standardize columns
+df.columns = df.columns.str.strip()
 df.columns = df.columns.str.replace(" ", "_")
 
+# Remove duplicates
 df.drop_duplicates(inplace=True)
 
+# Handle missing values
+df.fillna("Unknown", inplace=True)
+
+# Save cleaned file
 df.to_csv("../datasets/clean_sales.csv", index=False)
 
-print("Data cleaned successfully")
+print("ETL Completed Successfully")
