22a9e00683804da8a8d202465918ff818e6b3f71
ARVIND <carvind35@gmail.com>
2026-06-08 10:26:52 +0530

    Add CustomerID, ProductID, Category to Sales table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,9 +1,11 @@
-
 CREATE TABLE Sales (
     OrderID VARCHAR(50),
     OrderDate DATE,
+    CustomerID VARCHAR(50),
     CustomerName VARCHAR(100),
+    ProductID VARCHAR(50),
     ProductName VARCHAR(200),
-    SalesAmount FLOAT,
-    Profit FLOAT
+    Category VARCHAR(100),
+    SalesAmount DECIMAL(18,2),
+    Profit DECIMAL(18,2)
 );
